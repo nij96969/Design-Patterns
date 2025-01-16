071d79b760e91d411d146b258bb77e62f3ea096f
nij96969 <nijakbari@gmail.com>
2025-01-16 20:00:17 +0530

    Fly Weight Design Pattern

--- Structural Design Pattern/Fly Weight/CLientConnection.java
+++ Structural Design Pattern/Fly Weight/CLientConnection.java
@@ -1,22 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
 interface Connection {
     void connect(String extrinstic_state);
 }
 
 class ConcreteConnection implements Connection{
-    
-    ConcreteConnection(){
+    private final String server_address;
+    private final Integer port;
 
+    ConcreteConnection(String server_address , Integer port){
+        this.server_address = server_address;
+        this.port = port;
     }
 
     @Override
     public void connect(String extrinstic_state) {
-        // TODO Auto-generated method stub
+        System.out.println("Connecting to " + server_address + " at port : " + port + " with Session " + extrinstic_state);
         
+    }
+}
+
+class ConnectionPool {
+    private final Map<String , Connection> pool = new HashMap<>();
+
+    public Connection getConnection(String server_address , Integer port){
+        String key = server_address + ":" + port.toString();
+
+        if(!pool.containsKey(key))
+            pool.put(key, new ConcreteConnection(server_address , port));
+
+        return pool.get(key);
     }
 }
 
 public class CLientConnection {
     public static void main(String[] args) {
+        ConnectionPool pool = new ConnectionPool();
         
+        Connection conn1 = pool.getConnection("192.168.1.1", 8080);
+        conn1.connect("UserSession1");
+        
+        Connection conn2 = pool.getConnection("192.168.1.1", 8080);
+        conn2.connect("UserSession2");
+        
+        System.out.println(conn1 == conn2);
+        
+        Connection conn3 = pool.getConnection("192.168.1.2", 9090);
+        conn3.connect("UserSession3");
+        
+        System.out.println(conn1 == conn3);
     }
 }
